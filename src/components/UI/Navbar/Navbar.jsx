840d2e17b41e6f5c8aa0fe4da7459004644e300e
Elena <vodavkolodze@gmail.com>
2024-09-24 10:37:36 +0700

    add styles

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -12,12 +12,14 @@ const Navbar = (props) => {
     }
   return (
     <div className="navbar">
-        <MyButton onClick={logout}>
-            Выйти
-        </MyButton>
-            <div className="navbar__links">
-                <Link to="/about">О сайте</Link>
-                <Link to="/posts">Посты</Link>
+        <div class="container">
+            <MyButton onClick={logout}>
+                Выйти
+            </MyButton>
+                <div className="navbar__links">
+                    <Link to="/about">О сайте</Link>
+                    <Link to="/posts">Посты</Link>
+                </div>
             </div>
         </div>
   )

1f2527cf72baceec9c712e5789bed851ebbac06f
Elena <vodavkolodze@gmail.com>
2024-09-29 22:59:54 +0700

    fix links

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,11 @@ import Login from "../pages/Login";
 
 
 export const privateRoutes = [
-    {path: './about', element: <About />, exact: true},
-    {path: './posts', element: <Posts />, exact: true},
-    {path: './posts/:id', element: <PostIdPage />, exact: true},
+    {path: '/react-app/about', element: <About />, exact: true},
+    {path: '/react-app/posts', element: <Posts />, exact: true},
+    {path: '/react-app/posts/:id', element: <PostIdPage />, exact: true},
     {/*path: "*", element: <Error />, exact: true*/},
 ]
 export const publicRoutes = [
-    {path: './login', element: <Login />, exact: true},
+    {path: '/react-app/login', element: <Login />, exact: true},
 ]
